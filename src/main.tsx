87e1a70049393a0d4b23848276a472a695ef54a3
phil-lumin <philmaurer@lumin.co.uk>
2025-03-25 11:10:41 +0000

    Add basic favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './main.css'
 import App from './app'
 import { RecoilRoot } from 'recoil'
+import './main.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
