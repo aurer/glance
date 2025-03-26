c5927bf0a4dfb7c0241250b2a173811e087f89a3
Phil Maurer <philmau@gmail.com>
2025-03-26 09:54:08 +0000

    Fix form input validation

--- src/components/coin-form.tsx
+++ src/components/coin-form.tsx
@@ -25,118 +25,127 @@ import {
 import { Input } from "~/components/ui/input";
 
 const formSchema = z.object({
-  coin: z.string().min(1, "Coin name is required"),
-  amount: z.number().min(0, "Amount must be positive")
-});
+	coin: z.string().min(1, 'Coin name is required').toLowerCase(),
+	amount: z.number({ coerce: true, message: 'Must be a number' }).min(0, 'Must be positive'),
+})
 
-type Coin = z.infer<typeof formSchema>;
+type Coin = z.infer<typeof formSchema>
 
 export function CoinForm() {
-  const [editor, setEditor] = useAtom(editorAtom);
-  const setCoins = useSetAtom(coinsAtom);
-  const mode = editor?.index === -1 ? "add" : "edit";
+	const [editor, setEditor] = useAtom(editorAtom)
+	const setCoins = useSetAtom(coinsAtom)
+	const mode = editor?.index === -1 ? 'add' : 'edit'
 
-  const form = useForm<Coin>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      coin: "",
-      amount: 0
-    }
-  });
+	const form = useForm<Coin>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			coin: '',
+			amount: 0,
+		},
+	})
 
-  const onSubmit = (data: Coin) => {
-    if (mode === "edit" && editor !== null) {
-      setCoins(prevCoins => 
-        prevCoins.map((coin, index) => 
-          index === editor.index ? data : coin
-        )
-      );
-      setEditor(null);
-    } else {
-      setCoins(prevCoins => [...prevCoins, data]);
-    }
-    form.reset();
-    setEditor(null);
-  }
+	const onSubmit = (data: Coin) => {
+		if (mode === 'edit' && editor !== null) {
+			setCoins((prevCoins) =>
+				prevCoins.map((coin, index) => (index === editor.index ? data : coin))
+			)
+			setEditor(null)
+		} else {
+			setCoins((prevCoins) => [...prevCoins, data])
+		}
+		form.reset()
+		setEditor(null)
+	}
 
-  const handleOpenChange = (open: boolean) => {
-    if (!open) {
-      setEditor(null);
-    }
-  }
+	const handleOpenChange = (open: boolean) => {
+		if (!open) {
+			setEditor(null)
+		}
+	}
 
-  const handleDelete = () => {
-    if (editor !== null) {
-      setCoins(prevCoins => prevCoins.filter((_, index) => index !== editor.index));
-      setEditor(null);
-    }
-  }
+	const handleDelete = () => {
+		if (editor !== null) {
+			setCoins((prevCoins) => prevCoins.filter((_, index) => index !== editor.index))
+			setEditor(null)
+		}
+	}
 
-  useEffect(() => {
-    if (editor !== null) {
-      form.reset(editor.coin);
-    }
-  }, [editor, form]);
+	useEffect(() => {
+		if (editor !== null) {
+			form.reset(editor.coin)
+		}
+	}, [editor, form])
 
-  return (
-    <Dialog open={editor !== null} onOpenChange={handleOpenChange}>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>{mode === "add" ? "Add a Coin" : "Edit " + editor?.coin.coin}</DialogTitle>
-          <DialogDescription>
-            {mode === "add" ? "Add a coin to your portfolio" : "Edit the details of a coin in your portfolio"}
-          </DialogDescription>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="coin"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Coin</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter coin name" {...field} autoComplete="off" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+	return (
+		<Dialog open={editor !== null} onOpenChange={handleOpenChange}>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>
+						{mode === 'add' ? 'Add a Coin' : 'Edit ' + (editor?.coin.coin ?? '')}
+					</DialogTitle>
+					<DialogDescription>
+						{mode === 'add'
+							? 'Add a coin to your portfolio'
+							: 'Edit the details of a coin in your portfolio'}
+					</DialogDescription>
+				</DialogHeader>
+				{Boolean(editor) && (
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+							<FormField
+								control={form.control}
+								name="coin"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Coin</FormLabel>
+										<FormControl>
+											<Input
+												placeholder="Enter coin name"
+												{...field}
+												autoComplete="off"
+												autoCapitalize="none"
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
 
-            <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Amount</FormLabel>
-                  <FormControl>
-                    <Input 
-                      type="text" 
-                      inputMode="decimal"
-                      pattern="[0-9]*[.,]?[0-9]*"
-                      placeholder="Enter amount" 
-                      {...field}
-                      autoComplete="off"
-                      onChange={e => field.onChange(Number(e.target.value))}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <DialogFooter>
-              <div className="flex gap-2 justify-between w-full">
-                {mode === "edit" && (
-                  <Button type="button" onClick={handleDelete} variant="destructive">Delete</Button>
-                )}
-                <Button type="submit" className="ml-auto">
-                  {mode === "add" ? "Add" : "Save"}
-                </Button>
-              </div>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
-  )
+							<FormField
+								control={form.control}
+								name="amount"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Amount</FormLabel>
+										<FormControl>
+											<Input
+												type="text"
+												inputMode="decimal"
+												placeholder="Enter amount"
+												{...field}
+												autoComplete="off"
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<DialogFooter>
+								<div className="flex gap-2 justify-between w-full">
+									{mode === 'edit' && (
+										<Button type="button" onClick={handleDelete} variant="destructive">
+											Delete
+										</Button>
+									)}
+									<Button type="submit" className="ml-auto">
+										{mode === 'add' ? 'Add' : 'Save'}
+									</Button>
+								</div>
+							</DialogFooter>
+						</form>
+					</Form>
+				)}
+			</DialogContent>
+		</Dialog>
+	)
 } 
--- src/components/coin-list.tsx
+++ src/components/coin-list.tsx
@@ -9,31 +9,41 @@ export function CoinList() {
   const coinsData = useAtomValue(coinsDataAtom)
   const setEditor = useSetAtom(editorAtom);
 
+  const handleAddCoin = () => {
+		setEditor({ coin: { coin: '', amount: 0 }, index: -1 })
+	}
+
+	const handleEditCoin = (index: number) => () => {
+		setEditor({ coin: coins[index], index })
+	}
+
   return (
-    <div>
-      <ul className="mb-4">
-        {coins.map((coin, index) => (
-          <li key={index} className="flex gap-2 items-start justify-between mb-2" onClick={() => setEditor({ coin, index })}>
-          <span>
-            {coin.coin}
-            <span className="block text-xs text-gray-500">
-              {coin.amount.toFixed(4)}
-            </span>
-          </span>
-          <span className="text-right">
-            &pound;{((coinsData[coin.coin]?.gbp ?? 0) * coin.amount).toFixed(2)}
-          </span>
-        </li>
-      ))}
-      </ul>
-      <Button
-        variant="outline"
-        className="w-full mx-auto flex items-center justify-center gap-2" 
-        onClick={() => setEditor({ coin: { coin: "", amount: 0 }, index: -1 })}
-      >
-        <PlusIcon className="size-4" />
-        Add a Coin
-      </Button>
-    </div>
-  )
+		<div>
+			<ul className="mb-4">
+				{coins.map((coin, index) => (
+					<li
+						key={index}
+						className="flex gap-2 items-start justify-between mb-2"
+						onClick={handleEditCoin(index)}
+					>
+						<span>
+							{coin.coin}
+							<span className="block text-xs text-gray-500">{coin.amount.toFixed(4)}</span>
+						</span>
+						<span className="text-right">
+							&pound;{((coinsData[coin.coin]?.gbp ?? 0) * coin.amount).toFixed(2)}
+						</span>
+					</li>
+				))}
+			</ul>
+			<Button
+				variant="outline"
+				className="w-full mx-auto flex items-center justify-center gap-2"
+				onClick={handleAddCoin}
+			>
+				<PlusIcon className="size-4" />
+				Add a Coin
+			</Button>
+		</div>
+	)
 }
